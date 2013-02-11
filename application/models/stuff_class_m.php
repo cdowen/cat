b30a60edc5adc1d5e05237049793c4887b8d8456
cdowen <hhylx1950@gmail.com>
2013-02-11 14:59:31 +0800

    new file:   application/models/stuff_class_m.php 	modified:   application/models/stuff_m.php

---
+++ application/models/stuff_class_m.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class stuff_class_m extends CI_Model {
+  
+}
--- application/models/stuff_m.php
+++ application/models/stuff_m.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-//TODO : 权限控制
+/**
+*员工相关数据
+*/
 
 class stuff_m extends CI_model {
   
@@ -16,6 +18,7 @@ class stuff_m extends CI_model {
   function get_by_id($id){
     $result = $this->db
       ->where(array('id' => $id))
+      ->limit(1)
       ->get('staff');
 
     if ($result->num_rows() > 0)
@@ -33,6 +36,7 @@ class stuff_m extends CI_model {
   function get_by_name($name){
     $result = $this->db
       ->where(array('name' => $name))
+      ->limit(1)
       ->get('staff');
       
     if ($result->num_rows() > 0)
@@ -41,7 +45,28 @@ class stuff_m extends CI_model {
       return null;
   }
   
-  function create($name, $pwd, $power) {
+  /**
+  *创建员工
+  *@param type $name, 员工姓名
+  *@param type $pwd, 密码
+  *@param type $type, 员工类别，1,2,3三种
+  */
+  
+  function create($name, $pwd, $type) {
+    $data = array('name'=>$name, 'pass'=>sha1($pwd), 'class'=>$type);
     $result = $this->db
-      ->insert()
+      ->insert('staff', $data);
+  }
+  
+  /**
+  *根据id删除员工
+  *
+  *@param type $id
+  */
+  
+  function delete($id) {
+    $this->db
+      ->where('id'=>$id)
+      ->limit(1)
+      ->delete('staff');
   }
